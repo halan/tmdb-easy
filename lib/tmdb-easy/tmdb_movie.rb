f3aa92260b391bc26a34803c6114dccb0bc03678
Bruno Henrique <squall.bruno@gmail.com>
2011-12-01 23:50:45 -0200

    add method Movie.search

--- lib/tmdb-easy/tmdb_movie.rb
+++ lib/tmdb-easy/tmdb_movie.rb
@@ -1,7 +1,11 @@
 module TmdbEasy
   class TmdbMovie < TmdbBase
-    def movie_search(lang, response, api_key, keyword)
-      url = @@api_url + "Movie.search/"
+    def movie_search(keyword, lang = "en", response = "json")
+      method_request = @@api_url + "Movie.search/#{lang}/#{response}/#{self.api_key}/#{keyword}"
+
+      uri = URI.parse(method_request)
+      response = Net::HTTP.get_response(uri).body
+      @json_response = response.to_json
     end
   end
 end
--- lib/tmdb_easy.rb
+++ lib/tmdb_easy.rb
@@ -1,5 +1,9 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 require_files = []
-require_files.concat Dir[File.join(File.dirname(__FILE__), 'tmdb-easy', '*.rb')]
+require_files.concat Dir[File.join(File.dirname(__FILE__), 'tmdb-easy', '*.rb')].reverse!
 
 require_files.each do |file|
   require File.expand_path(file)
