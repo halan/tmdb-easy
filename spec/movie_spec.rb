fbcfd024257529e1a0d3d91704715b8cf9f2fa2c
Halan Pinheiro <halan.pinheiro@gmail.com>
2012-05-17 08:23:00 -0300

    Improving specs and added spec for search on xml

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -7,18 +7,31 @@ describe TmdbEasy::Movie do
     subject           { TmdbEasy::Movie }
     before            { subject.api_key = api_key }
 
-    it 'should request to http://api.themoviedb.org/2.1/Movie.search/ with keyword api_key when search and response' do
-      keyword    = 'zoombie'
+    describe 'Responses when request to http://api.themoviedb.org/2.1/Movie.search/ with keyword and api_key' do
+      let(:keyword)   { 'zoombie' }
+      let(:url_base)  { 'http://api.themoviedb.org/2.1/Movie.search/' }
 
-      Net::HTTP.should_receive(:get_response) do |url|
-        url.to_s.should include(keyword)
-        url.to_s.should include('http://api.themoviedb.org/2.1/Movie.search/')
-        url.to_s.should include(api_key)
-      end.and_return(stub(:body => '{"i am": "a zoombie"}'))
+      def get_response_should options
+        options.merge :url_include => [], :and_response => ''
+        Net::HTTP.should_receive(:get_response) do |url|
+          options[:url_include].each do |text|
+            url.to_s.should include(text)
+          end
+        end.and_return(stub(:body => options[:and_response]))
+      end
 
-      response = subject.search keyword
+      it 'should response with json' do
+        get_response_should :url_include => [keyword, url_base, api_key],
+                            :and_response => '{"i am": "a zoombie"}'
+        subject.search(keyword)['i am'].should == 'a zoombie'
+      end
 
-      response['i am'].should == 'a zoombie'
+      it 'should response with xml' do
+        get_response_should :url_include => [keyword, url_base, api_key],
+                            :and_response => '<movie>im a zoombie</movie>'
+        subject.search(keyword, 'en', 'xml').first.text.should == 'im a zoombie'
+      end
+
     end
   end
 
