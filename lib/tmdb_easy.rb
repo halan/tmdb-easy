008ed243c19c2734b1540172b2f725b07cd2155a
Halan Pinheiro <halan.pinheiro@gmail.com>
2012-05-16 23:50:29 -0300

    Cuting off the load of version.rb from manifest file

--- lib/tmdb_easy.rb
+++ lib/tmdb_easy.rb
@@ -1,6 +1,10 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
 require 'net/http'
 require 'uri'
-require 'json'
 require "rexml/document"
 
 #Load files in the correct order, not in alphabetical natural order. :)
@@ -9,8 +13,7 @@ required_files = ['base.rb',
                   'media.rb',
                   'misc.rb',
                   'movie.rb',
-                  'people.rb',
-                  'version.rb']
+                  'people.rb']
 
 required_files.each do |file|
   require File.expand_path(File.join(File.dirname(__FILE__), 'tmdb_easy', file))
