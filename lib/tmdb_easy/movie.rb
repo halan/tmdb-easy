0daab7dd13d0341352c9c50424d2e7bd519a8d9b
Halan Pinheiro <halan.pinheiro@gmail.com>
2012-07-25 10:41:57 -0300

    Refactoring movie.rb

--- lib/tmdb_easy/movie.rb
+++ lib/tmdb_easy/movie.rb
@@ -1,21 +1,33 @@
 module TmdbEasy  
   class Movie < Base
-    def self.search(keyword, lang = "en", response = "json")
-      if response == "json"
-        method_request = @@api_url + "Movie.search/#{lang}/#{response}/#{@@api_key}/#{keyword}"
+    def self.search(keyword, lang = :en, response_type = :json)
+      raise ArgumentError.new 'invalid lang' unless all_langs.include? lang.to_sym
 
-        uri = URI.parse(method_request)
-        http_response = Net::HTTP.get_response(uri).body
-        json_response = JSON.parse(http_response)
-
-      elsif response == "xml"
-        method_request = @@api_url + "Movie.search/#{lang}/#{response}/#{@@api_key}/#{keyword}"
-
-        uri = URI.parse(method_request)
-        http_response = Net::HTTP.get_response(uri).body
-        xml_response = REXML::Document.new(http_response)
-        xml_response.elements.to_a("//movie")
+      request keyword, lang, response_type do |response|
+        case response_type.to_sym
+          when :json  then response_with_json response
+          when :xml   then response_with_xml  response
+          else raise ArgumentError.new 'response should be :json or :xml'
+        end
       end
+    end
+
+    protected
+    def self.all_langs
+      [:en, :pt, :es]
+    end
+
+    def self.response_with_xml response
+      REXML::Document.new(response).elements.to_a("//movie")
+    end
+
+    def self.response_with_json response
+      JSON.parse(response)
+    end
+
+    def self.request keyword, lang, response_type
+      uri = URI.parse "#{@@api_url}Movie.search/#{lang}/#{response_type}/#{@@api_key}/#{keyword}"
+      yield Net::HTTP.get_response(uri).body 
     end
   end
 end
